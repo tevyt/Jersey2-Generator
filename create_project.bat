090772967718169899278a540dec54bc554a605b
Travis Smith <smithtal@jncb.com>
2016-12-21 10:02:09 -0500

    Determines appropriate archetype

--- create_project.bat
+++ create_project.bat
@@ -1,5 +1,13 @@
-set /p ProjectSelection  "Select a project type\n
-                         1. Grizzly Project
-                         2. JavaEE project
-                         3. Heroku Deployable"
-ECHO %ProjectSelection%
+@echo off
+echo Select a project type
+echo 1.Grizzly
+echo 2.JavaEE
+echo 3.Heroku
+set /P ProjectSelection= : %=%
+IF %ProjectSelection%==1 set Archetype=jersey-quickstart-grizzly2
+
+IF %ProjectSelection%==2 set Archetype=jersey-quickstart-webapp
+
+IF %ProjectSelection%==3 set Archetype=jersey-heroku-webapp
+
+echo %Archetype%
