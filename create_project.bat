5f184c88da38ed20c470e5a612797e1a45883b2e
Travis Smith <smithtal@jncb.com>
2016-12-21 09:31:24 -0500

    Created project

---
+++ create_project.bat
@@ -0,0 +1,5 @@
+set /p ProjectSelection  "Select a project type\n
+                         1. Grizzly Project
+                         2. JavaEE project
+                         3. Heroku Deployable"
+ECHO %ProjectSelection%
