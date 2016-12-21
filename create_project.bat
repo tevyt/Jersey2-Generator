9984eed0b890db42dac24bd294fc45380dbffd55
Travis Smith <smithtal@jncb.com>
2016-12-21 10:15:33 -0500

    Added maven command

--- create_project.bat
+++ create_project.bat
@@ -4,10 +4,12 @@ echo 1.Grizzly
 echo 2.JavaEE
 echo 3.Heroku
 set /P ProjectSelection= : %=%
+set Archetype=INV
 IF %ProjectSelection%==1 set Archetype=jersey-quickstart-grizzly2
-
 IF %ProjectSelection%==2 set Archetype=jersey-quickstart-webapp
-
 IF %ProjectSelection%==3 set Archetype=jersey-heroku-webapp
-
-echo %Archetype%
+IF %Archetype%==INV (echo Invalid selection & EXIT)
+set /P GroupId= groupId=%=%
+set /P ArtifactId= artifactId=%=%
+set /P Package= package=%=%
+mvn archetype:generate -DarchetypeArtifactId=%Archetype% -DarchetypeGroupId=org.glassfish.jersey.archetypes -DinteractiveMode=false -DgroupId=%GroupId% -DartifactId=%ArtifactId% -Dpackage=%Package% -DarchetypeVersion=2.25
